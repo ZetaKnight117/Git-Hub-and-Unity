464f69e7bcd4d5206dee7d69d27fab91ed7d40a0
ZetaKnight117 <61948380+ZetaKnight117@users.noreply.github.com>
2020-10-03 16:49:25 -0500

    new scripts added

---
+++ Assets/Scripts/RigidBodyCharacterController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float accerationForce = 10;
+
+    [SerializeField]
+    private float maxSpeed = 2f;
+
+    [SerializeField]
+    private PhysicMaterial stoppingPhysicMaterial, movingPhysicsMaterial;
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+    private new Collider collider;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDirection = new Vector3(input.x, 0, input.y);
+
+        collider.material = inputDirection.magnitude > 0 ? movingPhysicsMaterial : stoppingPhysicMaterial;
+      
+        if(rigidbody.velocity.magnitude < maxSpeed)
+        {
+          rigidbody.AddForce(inputDirection * accerationForce, ForceMode.Acceleration);
+        }       
+       
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+}
