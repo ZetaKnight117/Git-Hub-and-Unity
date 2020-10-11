d118bb9aad0b4af2c00ce61a5cffd1fa2d3240cf
ZetaKnight117 <primemaster@outlook.com>
2020-10-10 21:17:48 -0500

    update
    update

--- Assets/Scripts/RigidBodyCharacterController.cs
+++ Assets/Scripts/RigidBodyCharacterController.cs
@@ -9,29 +9,21 @@ public class RigidBodyCharacterController : MonoBehaviour
 
     [SerializeField]
     private float maxSpeed = 2f;
-
-    [SerializeField]
-    private PhysicMaterial stoppingPhysicMaterial, movingPhysicsMaterial;
-
     private new Rigidbody rigidbody;
     private Vector2 input;
-    private new Collider collider;
 
     private void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        collider = GetComponent<Collider>();
     }
 
     private void FixedUpdate()
     {
         var inputDirection = new Vector3(input.x, 0, input.y);
 
-        collider.material = inputDirection.magnitude > 0 ? movingPhysicsMaterial : stoppingPhysicMaterial;
-      
         if(rigidbody.velocity.magnitude < maxSpeed)
         {
-          rigidbody.AddForce(inputDirection * accerationForce, ForceMode.Acceleration);
+            rigidbody.AddForce(inputDirection * accerationForce, ForceMode.Acceleration);
         }       
        
     }
