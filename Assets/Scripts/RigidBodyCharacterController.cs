f904ddcbff5082ce071b25f1cdf99494750e03d2
ZetaKnight117 <primemaster@outlook.com>
2020-10-23 00:04:55 -0500

    camera fixed

--- Assets/Scripts/RigidBodyCharacterController.cs
+++ Assets/Scripts/RigidBodyCharacterController.cs
@@ -9,9 +9,13 @@ public class RigidBodyCharacterController : MonoBehaviour
     private float accerationForce = 10;
 
     [SerializeField]
+    private PhysicMaterial stoppingPhysicsMaterial, movingPhysicalMaterial;
+
+    [SerializeField]
     private float maxSpeed = 2f;
     private new Rigidbody rigidbody;
     private Vector2 input;
+    private object cameraRelativeInputDirection;
 
     private void Start()
     {
@@ -21,12 +25,21 @@ public class RigidBodyCharacterController : MonoBehaviour
     private void FixedUpdate()
     {
         var inputDirection = new Vector3(input.x, 0, input.y);
+       
+        Vector3 cameraFlattenedForward = Camera.main.transform.forward;
+        cameraFlattenedForward.y = 0;
+        var cameraRotation = Quaternion.LookRotation(cameraFlattenedForward);
 
+        Vector3 cameraRelativeInputDirection = cameraRotation * inputDirection;
         if(rigidbody.velocity.magnitude < maxSpeed)
         {
             rigidbody.AddForce(inputDirection * accerationForce, ForceMode.Acceleration);
-        }       
-       
+        } 
+        
+       //if (cameraRelativeInputDirection.magnitude > 0) 
+        //{
+          //  var target
+        //}
     }
 
     public void OnMove(InputAction.CallbackContext context)
